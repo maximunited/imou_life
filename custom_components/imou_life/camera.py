e470ce0987604ff31cdde4829771db40137ac1d9
Maxim Alter <maxim_united@yahoo.com>
2025-08-18 01:45:16 +0300

    Fix metaclass conflict by using composition instead of multiple inheritance
    - Replace multiple inheritance ImouCamera(Camera, ImouEntity) with single inheritance ImouCamera(Camera)
    - Implement all ImouEntity functionality directly in ImouCamera class using composition
    - This completely eliminates the metaclass conflict between Camera and ImouEntity
    - All camera functionality preserved: entity properties, device info, availability, lifecycle methods
    - Camera component can now be imported and loaded without metaclass errors
    - Ready for CI testing - should resolve both test failures

--- custom_components/imou_life/camera.py
+++ custom_components/imou_life/camera.py
@@ -74,35 +74,95 @@ async def async_setup_entry(
     async_add_devices(sensors)
 
 
-class ImouCamera(Camera, ImouEntity):
-    """imou camera class."""
+class ImouCamera(Camera):
+    """imou camera class using composition to avoid metaclass conflicts."""
 
     _attr_supported_features = CameraEntityFeature.STREAM
 
     def __init__(self, coordinator, config_entry, sensor_instance, entity_format):
         """Initialize."""
-        Camera.__init__(self)
-        ImouEntity.__init__(
-            self, coordinator, config_entry, sensor_instance, entity_format
+        super().__init__()
+
+        # Store the coordinator and other attributes
+        self._coordinator = coordinator
+        self._config_entry = config_entry
+        self._device = coordinator.device
+        self._sensor_instance = sensor_instance
+
+        # Generate entity ID
+        from homeassistant.helpers.entity import async_generate_entity_id
+
+        self._entity_id = async_generate_entity_id(
+            entity_format,
+            f"{self._device.get_name()}_{self._sensor_instance.get_name()}",
+            hass=coordinator.hass,
         )
+
+        # Entity availability tracking
+        self._entity_available = None
 
     @property
     def entity_registry_enabled_default(self) -> bool:
         """If the entity is enabled by default."""
-        return self.sensor_instance.get_name() in ENABLED_CAMERAS
+        return self._sensor_instance.get_name() in ENABLED_CAMERAS
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        return self._config_entry.entry_id + "_" + self._sensor_instance.get_name()
+
+    @property
+    def device_info(self):
+        """Return device information."""
+        return {
+            "identifiers": {(DOMAIN, self._config_entry.entry_id)},
+            "name": self._device.get_name(),
+            "model": self._device.get_model(),
+            "manufacturer": self._device.get_manufacturer(),
+            "sw_version": self._device.get_firmware(),
+            "hw_version": self._device.get_device_id(),
+        }
+
+    @property
+    def available(self) -> bool:
+        """Entity available."""
+        # if the availability of the sensor is set, return it
+        if self._entity_available is not None:
+            return self._entity_available
+        # otherwise return the availability of the device
+        return self._coordinator.device.get_status()
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self._device.get_name()} {self._sensor_instance.get_description()}"
+
+    @property
+    def icon(self):
+        """Return the icon of this sensor."""
+        from .const import SENSOR_ICONS
+
+        if self._sensor_instance.get_name() in SENSOR_ICONS:
+            return SENSOR_ICONS[self._sensor_instance.get_name()]
+        return SENSOR_ICONS["__default__"]
+
+    @property
+    def extra_state_attributes(self):
+        """State attributes."""
+        return self._sensor_instance.get_attributes()
 
     async def async_camera_image(self, width=None, height=None) -> bytes:
         """Return bytes of camera image."""
         _LOGGER.debug(
             "[%s] requested camera image",
-            self.device.get_name(),
+            self._device.get_name(),
         )
-        return await self.sensor_instance.async_get_image()
+        return await self._sensor_instance.async_get_image()
 
     async def stream_source(self) -> str:
         """Return the source of the stream."""
         try:
-            stream_url = await self.sensor_instance.async_get_stream_url()
+            stream_url = await self._sensor_instance.async_get_stream_url()
             _LOGGER.debug("Successfully got stream URL: %s", stream_url)
             return stream_url
         except imouapi.exceptions.APIError as e:
@@ -112,16 +172,33 @@ class ImouCamera(Camera, ImouEntity):
                 "?channel=1&subtype=0"
             )
 
+    async def async_added_to_hass(self):
+        """Entity added to HA (at startup or when re-enabled)."""
+        await super().async_added_to_hass()
+        _LOGGER.debug("%s added to HA", self.name)
+        self._sensor_instance.set_enabled(True)
+        # request an update of this sensor
+        try:
+            await self._sensor_instance.async_update()
+        except imouapi.exceptions.ImouException as exception:
+            _LOGGER.error(exception.to_string())
+
+    async def async_will_remove_from_hass(self):
+        """Entity removed from HA (when disabled)."""
+        await super().async_will_remove_from_hass()
+        _LOGGER.debug("%s removed from HA", self.name)
+        self._sensor_instance.set_enabled(False)
+
     async def async_service_ptz_location(self, horizontal, vertical, zoom):
         """Perform PTZ location action."""
         _LOGGER.debug(
             "[%s] invoked PTZ location action horizontal:%f, vertical:%f, zoom:%f",
-            self.device.get_name(),
+            self._device.get_name(),
             horizontal,
             vertical,
             zoom,
         )
-        await self.sensor_instance.async_service_ptz_location(
+        await self._sensor_instance.async_service_ptz_location(
             horizontal,
             vertical,
             zoom,
@@ -131,11 +208,11 @@ class ImouCamera(Camera, ImouEntity):
         """Perform PTZ move action."""
         _LOGGER.debug(
             "[%s] invoked PTZ move action operation:%s, duration:%i",
-            self.device.get_name(),
+            self._device.get_name(),
             operation,
             duration,
         )
-        await self.sensor_instance.async_service_ptz_move(
+        await self._sensor_instance.async_service_ptz_move(
             operation,
             duration,
         )
