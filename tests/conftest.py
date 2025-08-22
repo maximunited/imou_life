34ab5d33ef83416e61244ff3de64fe40616840d2
Maxim Alter <maxim_united@yahoo.com>
2025-08-23 01:07:48 +0300

    Remove frame helper mocks that don't exist in all HA versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -135,8 +135,6 @@ def mock_hass_components():
     """Mock Home Assistant components that are not available in tests."""
     with (
         patch("homeassistant.helpers.aiohttp_client.async_get_clientsession"),
-        patch("homeassistant.helpers.frame.report_usage"),
-        patch("homeassistant.helpers.frame.report_non_thread_safe_operation"),
         patch("homeassistant.components.zeroconf.async_get_async_zeroconf"),
         patch("homeassistant.components.zeroconf.usage.report_usage"),
     ):
