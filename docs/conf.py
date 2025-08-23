7bfe32817a09114edf98a75c2c8aa779f309d197
Maxim Alter <maxim_united@yahoo.com>
2025-08-24 01:45:02 +0300

    Temporarily remove sphinx_rtd_theme and update dependencies for better compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,13 @@ release = "1.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "myst_parser",
+]
 
 # The suffix of source filenames.
 source_suffix = {
-    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
 
 # The master toctree document.
@@ -30,5 +32,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.
-html_theme = "sphinx_rtd_theme"
+# Use the default Sphinx theme for now
+# html_theme = "sphinx_rtd_theme"
